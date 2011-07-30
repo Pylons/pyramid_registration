922bdc0be974bea6aa60fad4349baf97e43635b7
Niall O'Higgins <niallo@unworkable.org>
2011-07-30 00:19:21 -0600

    add some docs.

--- pyramid_registration/interfaces.py
+++ pyramid_registration/interfaces.py
@@ -5,24 +5,33 @@ class IRegistrationBackend(Interface):
     def __init__(self, config, settings):
         """ Must pass in configurator and settings objects """
 
-    def add_user(self, **kw):
-        """ Add a user to the storage
-        TODO: define params """
+    def add_user(self, struct):
+        """ Add a user to the storage.
 
-    def add_group(self, whatever):
-        """ Add a group to the storage
-        TODO: define params """
+        ``struct``
+        User structure to store. It is up to the backend implementation to
+        validate this structure. However, it is probably safe to assume it
+        supports keys username, password and email.
+        """
 
     def activate(self, token):
         """ Mark user as activated.
-        TODO: define params """
+
+        ``token``
+        The token linked to the account to activate. """
 
     def verify_access_token(self, token):
-        """ Verify access token
-        Return the user id if valid,
-        None otherwise. """
+        """ Verify whether access token is associated with a valid account and is not
+
+        ``token``
+        The token to verify.
+
+        Returns the user id if valid, None otherwise. """
 
     def issue_access_token(self, user_id):
         """ Generate a new random access token
         and associate it with the user_id in the database.
+
+        ``user_id``
+        The user_id of the account to issue the token for.
         """
