356c33d9eaf87159b96279e756f5ec01573f63d1
Niall O'Higgins <niallo@unworkable.org>
2011-07-30 16:34:10 -0600

    zap some crud

--- pyramid_registration/tests.py
+++ pyramid_registration/tests.py
@@ -219,21 +219,3 @@ class ViewTests(unittest.TestCase):
     def tearDown(self):
         testing.tearDown()
 
-
-
-class IntegrationTests(unittest.TestCase):
-    def setUp(self):
-        from pyramid_registration import main
-        # XXX make configurable
-        app = main({}, **{"mongodb.url":"mongodb://localhost",
-            "mongodb.db_name":test_db,
-            "mako.directories":"dragpushpullcom:templates"})
-        from webtest import TestApp
-        self.testapp = TestApp(app)
-        self.settings = app.registry.settings
-        self.db = self.settings['mongodb_conn'][self.settings["mongodb.db_name"]]
-
-    def tearDown(self):
-        """ Clear out the application registry """
-        testing.tearDown()
-        self.settings["mongodb_conn"].drop_database(test_db)
